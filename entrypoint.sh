7c533bd9103e7d49643cdeb0253d7751b8e75988
Nick Thurman <nick.thurman@digicert.com>
2024-02-01 14:48:05 -0700

    test 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,21 +10,18 @@ OUT=""
 #loop for deleting old branches
 for i in $(git for-each-ref refs/remotes/origin --sort=committerdate  --format='%(HEAD)%(color:yellow)%(refname:short)%(color:reset) %(color:green)%(committerdate:raw)%(color:reset)')
 do
-    if [ $i -eq "origin/KEEP"* ]
-    then 
-      continue
-    else
-      export IFS=$' '
-      elements=($i)
-    
-      if [ $TIME -gt ${elements[1]} ]
-      then
-          export IFS="/"
-	      inner_elements=(${elements[0]})
-          git push origin --delete ${inner_elements[1]}
-          #echo ${inner_elements[1]}
-          OUT="${OUT}, ${inner_elements[1]}"
-      fi
+    export IFS=$' '
+    elements=($i)    
+    if [ $TIME -gt ${elements[1]} ]
+    then
+        export IFS="/"
+	    inner_elements=(${elements[0]})
+        if [ ${inner_elements[1]} -ne "origin/KEEP"* ]
+        then
+            git push origin --delete ${inner_elements[1]}
+            #echo ${inner_elements[1]}
+            OUT="${OUT}, ${inner_elements[1]}"
+        fi
     fi
 done
 export IFS=$'\n'
